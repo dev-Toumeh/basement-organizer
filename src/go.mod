c263a8d9e7680ce8de47b356fb5d6e1af99478d7
toumeh <dev.toumeh@gmail.com>
2024-04-27 11:44:27 +0200

    add basic auth structure

---
+++ src/go.mod
@@ -0,0 +1,3 @@
+module basment/main
+
+go 1.22.1
---
+++ src/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Welcome to my website!")
+    })
+
+    http.HandleFunc("/login", func (w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Welcome to my login")
+    })
+
+    http.HandleFunc("/register", func (w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Welcome to my register")
+    })
+
+    http.ListenAndServe(":8000", nil)
+}
+
+
