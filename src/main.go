46e18cbff28d19353ec2ac26f4d84cce0346b8c6
Alex Korn <den.korn.alex@gmail.com>
2024-04-27 12:33:14 +0200

    Format with go fmt

--- src/main.go
+++ src/main.go
@@ -6,17 +6,15 @@ import (
 )
 
 func main() {
-    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Welcome to my website!")
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my website!")
+	})
 
 	http.HandleFunc("/login", login)
 
-    http.HandleFunc("/register", func (w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Welcome to my register")
-    })
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my register")
+	})
 
-    http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8000", nil)
 }
-
-
