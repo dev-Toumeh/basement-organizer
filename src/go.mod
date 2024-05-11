4159a964a9dd1286baf416e35233bc87612bfed5
toumeh <dev.toumeh@gmail.com>
2024-05-11 02:56:19 +0200

    add register functionality

--- src/go.mod
+++ src/go.mod
@@ -2,4 +2,7 @@ module basment/main
 
 go 1.22.1
 
-require golang.org/x/crypto v0.22.0 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+)
--- src/register.go
+++ src/register.go
@@ -1,32 +1,130 @@
 package main
 
 import (
-    "html/template"
-    "net/http"
-    "log"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+  "github.com/google/uuid"
+)
+
+const (
+	REGISTER_FAILED_MESSAGE string = "register failed"
+	Username                       = "username"
+	Password                       = "password"
 )
 
 type registerPage struct {
-
-  Title string
+	Title string
 }
 
-func register(response http.ResponseWriter, request *http.Request) {
-    if request.Method != "GET" {
-        http.Error(response, "Method not allowed", http.StatusMethodNotAllowed)
+type DBUsers []DBUser
+
+func register(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		NewUsername := r.PostFormValue(Username)
+		NewPassword := r.PostFormValue(Password)
+//		fmt.Printf("the usernmae is %s \n and the password is %s", username, password)
+
+		if NewUsername == "" {
+			log.Println("Missing username")
+			fmt.Fprintln(w, REGISTER_FAILED_MESSAGE)
+			return
+		}
+		if NewPassword == "" {
+			log.Println("Missing password")
+			fmt.Fprintln(w, REGISTER_FAILED_MESSAGE)
+			return
+		}
+		// check the Database
+
+		// open the json file
+		file, err := os.Open("users.json")
+		if err != nil {
+			log.Println("Error opening users.json:", err)
+			fmt.Fprintln(w, REGISTER_FAILED_MESSAGE)
+			return
+		}
+
+		// turn into bytes
+		byteValue, err := io.ReadAll(file)
+		if err != nil {
+			log.Println("Error happened while opening users.json file: ", err)
+			fmt.Fprintln(w, REGISTER_FAILED_MESSAGE)
+			return
+		}
+
+		defer file.Close()
+
+		// convert json File to array of DBuser.
+		var dbUsers DBUsers
+		err = json.Unmarshal(byteValue, &dbUsers)
+		if err != nil {
+			log.Println("Error reading users.json:", err)
+			fmt.Fprintln(w, REGISTER_FAILED_MESSAGE)
+			return
+		}
+
+		//check the if the username is exist
+		for _, dbUser := range dbUsers {
+			if dbUser.Username == NewUsername {
+				log.Printf("the username %s is already token", NewUsername)
+				fmt.Fprintln(w, REGISTER_FAILED_MESSAGE)
         return
+			}
+		}
+    
+    // hash the password
+    NewHashedPassword, err := HashPassword(NewPassword)
+    if err != nil{
+				log.Fatal(err)
+				fmt.Fprintln(w, REGISTER_FAILED_MESSAGE)
+    }  
+    // create new Record
+    newUser := DBUser {
+      Id: uuid.New(),
+      Username: NewUsername,
+      PassHash: NewHashedPassword,
     }
-    tmpl, err := template.ParseFiles("./register.html")
-    if err != nil {
-        http.Error(response, "Error loading template", http.StatusInternalServerError)
-        return
-    }
-    data := registerPage{
-        Title: "Register",
-    }
-    err = tmpl.Execute(response, data)
-    if err != nil {
-        log.Printf("Error executing template: %v", err)
-        http.Error(response, "Error rendering page", http.StatusInternalServerError)
-    }
+
+    dbUsers = append(dbUsers, newUser)
+
+    // Convert the updated users list back to JSON
+    updatedJSON, err := json.Marshal(dbUsers)
+		if err != nil {
+			log.Println("Error marshalling new user data:", err)
+			fmt.Fprintln(w, REGISTER_FAILED_MESSAGE)
+			return
+		}
+    
+    // Write the updated JSON back to the File
+    err = os.WriteFile("users.json", updatedJSON, 0644)
+		if err != nil {
+			log.Println("Error writing to users.json:", err)
+			fmt.Fprintln(w, REGISTER_FAILED_MESSAGE)
+			return
+		}
+
+		log.Println("User registered successfully:", newUser.Username)
+		fmt.Fprintln(w, "User registered successfully")
+    
+    return
+	}
+
+	tmpl, err := template.ParseFiles("./register.html")
+	if err != nil {
+		http.Error(w, "Error loading template", http.StatusInternalServerError)
+		return
+	}
+	data := registerPage{
+		Title: "Register",
+	}
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		log.Printf("Error executing template: %v", err)
+		http.Error(w, "Error rendering page", http.StatusInternalServerError)
+	}
 }
