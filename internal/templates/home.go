93a6f7ba14125bee0412615d6f526645bd467218
Alex Korn <den.korn.alex@gmail.com>
2024-06-01 20:55:24 +0200

    Rename HomeHandler to HomePage

--- internal/templates/home.go
+++ internal/templates/home.go
@@ -15,7 +15,7 @@ const ROOT_PAGE_TEMPLATE string = "page"
 const ROOT_PAGE_TEMPLATE_FILENAME string = ROOT_PAGE_TEMPLATE + ".html"
 const ROOT_PAGE_TEMPLATE_FILE string = "internal/templates/" + ROOT_PAGE_TEMPLATE_FILENAME
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
+func HomePage(w http.ResponseWriter, r *http.Request) {
 
 	tmpl, err := template.ParseFiles(ROOT_PAGE_TEMPLATE_FILE, "internal/templates/home.html")
 	if err != nil {
