59ab8c6278776a2dec2bd441c42e59e0b30a2442
Alex Korn <den.korn.alex@gmail.com>
2024-06-09 13:00:15 +0200

    Update templates, add status bar, add javascript function to stop sending requests for the same page

--- internal/static/custom-functions.js
+++ internal/static/custom-functions.js
@@ -1,8 +1,20 @@
-document.getElementById('registerButton').addEventListener('click', function(event) {
-    var password = document.getElementById('password').value;
-    var confirmPassword = document.getElementById('password-confirm').value;
-    if (password !== confirmPassword) {
-        document.getElementById('response').innerText = 'Passwords do not match';
-        event.preventDefault(); // Prevent form from submitting
+//document.getElementById('registerButton').addEventListener('click', function(event) {
+//    var password = document.getElementById('password').value;
+//    var confirmPassword = document.getElementById('password-confirm').value;
+//    if (password !== confirmPassword) {
+//        document.getElementById('response').innerText = 'Passwords do not match';
+//        event.preventDefault(); // Prevent form from submitting
+//    }
+//});
+
+document.querySelectorAll("a")
+    .forEach(e => {
+        e.addEventListener('htmx:beforeRequest', stopRequestToSamePath)
+    })
+
+function stopRequestToSamePath(/** @type Event*/ event){
+    console.log(event.detail.pathInfo.requestPath, document.location.pathname)
+    if (event.detail.pathInfo.requestPath === document.location.pathname) {
+        event.preventDefault()
     }
-});
+}
