f505909fea6f6e2b18ac2267723a6a66a79e8638
Alex Korn <den.korn.alex@gmail.com>
2024-06-15 19:26:25 +0200

    Implement basic ReadData api to get an item from JsonDB

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,13 +1,17 @@
 package routes
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
 	"basement/main/internal/auth"
 )
 
 const (
-	STATIC string = "/static/"
+	STATIC           string = "/static/"
+	ITEMS_FILE_PATH  string = "internal/auth/items.json"
+	API_V1_READ_ITEM string = "/api/v1/read/item/id"
 )
 
 func RegisterRoutes(db auth.AuthDatabase) {
@@ -18,9 +22,17 @@ func RegisterRoutes(db auth.AuthDatabase) {
 	http.HandleFunc("/logout", auth.LogoutHandler)
 	http.HandleFunc("/api/v1/create/item", CreateItem)
 	http.HandleFunc("/api/v1/read/items", ReadItems)
-	http.HandleFunc("/api/v1/read/item/id", ReadItem)
+	http.HandleFunc(API_V1_READ_ITEM, ReadItem)
 	http.HandleFunc("/api/v1/update/item/id", UpdateItem)
 	http.HandleFunc("/api/v1/delete/item", DeleteItem)
+}
+
+// CreateItemsJsonDB creates DB instance by reading or creating "items.json" file from disk.
+func CreateItemsJsonDB() (*auth.JsonDB, error) {
+	db := &auth.JsonDB{}
+	db.InitItemsFromFile(ITEMS_FILE_PATH)
+	log.Println("")
+	return db, nil
 }
 
 func CreateItem(w http.ResponseWriter, r *http.Request) {
@@ -32,7 +44,10 @@ func ReadItems(w http.ResponseWriter, r *http.Request) {
 }
 
 func ReadItem(w http.ResponseWriter, r *http.Request) {
-	panic("unimplemented")
+	db, _ := CreateItemsJsonDB()
+	// get "Water Bottle"
+	fmt.Fprint(w, db.Items["123e4567-e89b-12d3-a456-426614174002"])
+	return
 }
 
 func DeleteItem(w http.ResponseWriter, r *http.Request) {
@@ -42,4 +57,3 @@ func DeleteItem(w http.ResponseWriter, r *http.Request) {
 func UpdateItem(w http.ResponseWriter, r *http.Request) {
 	panic("unimplemented")
 }
-
