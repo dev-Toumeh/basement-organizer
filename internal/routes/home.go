f1255bd4e9eb994b8010e084e7d5fcb16f04f78b
Alex Korn <den.korn.alex@gmail.com>
2024-07-06 12:07:52 +0200

    Update home-page template and replace ApplyPageTemplate with Render function

--- internal/routes/home.go
+++ internal/routes/home.go
@@ -11,11 +11,12 @@ import (
 // this is the first route inside of the application, so this page will open by Default
 func HomePage(w http.ResponseWriter, r *http.Request) {
 	authenticated, _ := auth.Authenticated(r)
-	data := templates.PageTemplate{
-		Title:         "home",
-		Authenticated: authenticated,
-	}
-	if err := templates.ApplyPageTemplate(w, "internal/templates/home.html", data); err != nil {
+	data := templates.NewPageTemplate()
+	data.Title = "home"
+	data.Authenticated = authenticated
+
+	err := templates.Render(w, "home-page", data)
+	if err != nil {
 		fmt.Fprintln(w, "failed")
 		return
 	}
