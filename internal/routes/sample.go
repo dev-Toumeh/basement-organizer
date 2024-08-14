407e6ff452452e30639fdc01c9228684bbaaba31
Alex Korn <den.korn.alex@gmail.com>
2024-08-14 17:07:09 +0200

    Add functions to trigger notifications from server via HX-Trigger header values

--- internal/routes/sample.go
+++ internal/routes/sample.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"basement/main/internal/auth"
 	"basement/main/internal/templates"
+	"fmt"
 	"net/http"
 )
 
@@ -13,5 +14,25 @@ func SamplePage(w http.ResponseWriter, r *http.Request) {
 	data.Authenticated = authenticated
 	data.User = auth.Username(r)
 
-	templates.Render(w, templates.TEMPLATE_SAMPLE_PAGE, data)
+	triggerAllServerNotifications(w)
+	MustRender(w, r, templates.TEMPLATE_SAMPLE_PAGE, data)
+}
+
+func triggerAllServerNotifications(w http.ResponseWriter) {
+	w.Header().Set("HX-Trigger-After-Swap", `{"ServerNotificationEvents":[{"message":"error", "type":"error" },{"message":"warning", "type":"warning" },{"message":"success", "type":"success" }, {"message":"default", "type":"" }]}`)
+}
+
+func TriggerErrorNotification(w http.ResponseWriter, message string) {
+	msg := fmt.Sprintf(`{"ServerNotificationEvents":[{"message":"%s", "type":"error" }]}`, message)
+	w.Header().Set("HX-Trigger-After-Swap", msg)
+}
+
+func TriggerSuccessNotification(w http.ResponseWriter, message string) {
+	msg := fmt.Sprintf(`{"ServerNotificationEvents":[{"message":"%s", "type":"success" }]}`, message)
+	w.Header().Set("HX-Trigger-After-Swap", msg)
+}
+
+func TriggerWarningNotification(w http.ResponseWriter, message string) {
+	msg := fmt.Sprintf(`{"ServerNotificationEvents":[{"message":"%s", "type":"warning" }]}`, message)
+	w.Header().Set("HX-Trigger-After-Swap", msg)
 }
