db4af6fc49be8f7f10ca01910ce9cf095f27872c
Alex Korn <den.korn.alex@gmail.com>
2024-08-18 17:05:45 +0200

    Implement Marshaler interface for Box to encode in json

--- internal/items/box.go
+++ internal/items/box.go
@@ -3,6 +3,7 @@ package items
 import (
 	"basement/main/internal/database"
 	"basement/main/internal/logg"
+	"encoding/json"
 	"fmt"
 	"slices"
 
@@ -25,14 +26,44 @@ type AnotherItem struct {
 type Box struct {
 	// Quantity    int64     `json:"quantity"    validate:"omitempty,numeric,gte=1"`
 	// Weight      string    `json:"weight"      validate:"omitempty,numeric"`
-	Id          uuid.UUID `json:"id"`
-	Label       string    `json:"label"       validate:"required,lte=128"`
-	Description string    `json:"description" validate:"omitempty,lte=256"`
-	Picture     string    `json:"picture"     validate:"omitempty,base64"`
-	QRcode      string    `json:"qrcode"      validate:"omitempty,alphanumunicode"`
-	Items       []*database.Item
-	InnerBoxes  []*Box
-	OuterBox    *Box
+	Id          uuid.UUID        `json:"id"`
+	Label       string           `json:"label"       validate:"required,lte=128"`
+	Description string           `json:"description" validate:"omitempty,lte=256"`
+	Picture     string           `json:"picture"     validate:"omitempty,base64"`
+	QRcode      string           `json:"qrcode"      validate:"omitempty,alphanumunicode"`
+	Items       []*database.Item `json:"items"`
+	InnerBoxes  []*Box           `json:"innerboxes"`
+	OuterBox    *Box             `json:"outerbox" `
+}
+
+type BoxC struct {
+	// Quantity    int64     `json:"quantity"    validate:"omitempty,numeric,gte=1"`
+	// Weight      string    `json:"weight"      validate:"omitempty,numeric"`
+	Id          uuid.UUID       `json:"id"`
+	Label       string          `json:"label"       validate:"required,lte=128"`
+	Description string          `json:"description" validate:"omitempty,lte=256"`
+	Picture     string          `json:"picture"     validate:"omitempty,base64"`
+	QRcode      string          `json:"qrcode"      validate:"omitempty,alphanumunicode"`
+	Items       []database.Item `json:"items"`
+	InnerBoxes  []Box           `json:"innerboxes"`
+	OuterBox    Box             `json:"outerbox" `
+}
+
+func (b *Box) MarshalJSON() ([]byte, error) {
+	c := BoxC{}
+	for _, item := range b.Items {
+		it := *item
+		c.Items = append(c.Items, it)
+	}
+
+	for _, innerb := range b.InnerBoxes {
+		c.InnerBoxes = append(c.InnerBoxes, *innerb)
+	}
+
+	// if b.OuterBox != nil {
+	// 	c.OuterBox = *b.OuterBox
+	// }
+	return json.Marshal(c)
 }
 
 func (box *Box) MoveTo(other any) error {
