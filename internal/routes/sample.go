fc74c7a9c90670b3189ee7e00e6635a0f7431c81
Alex Korn <den.korn.alex@gmail.com>
2024-09-30 17:53:50 +0200

    Add sample error chain

--- internal/routes/sample.go
+++ internal/routes/sample.go
@@ -2,8 +2,10 @@ package routes
 
 import (
 	"basement/main/internal/auth"
+	"basement/main/internal/logg"
 	"basement/main/internal/server"
 	"basement/main/internal/templates"
+	"errors"
 	"net/http"
 )
 
@@ -15,6 +17,11 @@ func SamplePage(w http.ResponseWriter, r *http.Request) {
 	data.Authenticated = authenticated
 	data.User = user
 
+	err := errors.New("Long error chain start !!!")
+	err2 := logg.Errorf("Error 2 %w", err)
+	err3 := logg.Errorf("Error 3 %w", err2)
+	err4 := logg.Errorf("Error 4 %w", err3)
+	logg.Err("Error happened:", err4)
 	server.TriggerAllServerNotifications(w)
 	server.MustRender(w, r, templates.TEMPLATE_SAMPLE_PAGE, data.Map())
 }
