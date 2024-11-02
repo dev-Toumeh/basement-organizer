e61a8e6064134fabcec4cfa5a4ade1f978fd5865
Alex Korn <den.korn.alex@gmail.com>
2024-11-02 12:38:03 +0100

    Add list template

---
+++ internal/items/list.go
@@ -0,0 +1,52 @@
+package items
+
+import (
+	"basement/main/internal/templates"
+	"net/http"
+)
+
+type ListTemplate struct {
+	FormHXGet   string // /boxes, /shelves
+	RowHXGet    string // hx-get="{{ .RowHXGet }}/{{.ID}}"
+	SearchInput any
+
+	Rows                              []ListRow
+	RowAction                         bool   // If an action button should be displayed inside each row instead of checkmarks.
+	RowActionHXPost                   string // hx-post="{{ .ActionHXPost }}"
+	RowActionHXPostWithID             string // hx-post="{{ .ActionHXPost }}/{{.ID}}"
+	RowActionHXPostWithIDAsQueryParam string // hx-post="{{ .ActionHXPost }}?id={{.ID}}"
+	RowActionName                     string // button and column header name
+	RowActionHXTarget                 string // hx-target="{{$RowActionHXTarget}}" in action button
+
+	AdditionalDataInputs      []DataInput
+	AdditionalDataInputValues []string
+	AdditionalDataInputName   string
+	ReturnSelectedRowInput    bool
+}
+
+type listTemplate2 struct {
+	ListTemplate
+	ActionInputs []map[string]string
+}
+
+type DataInput struct {
+	Key   string
+	Value string
+}
+
+func (tmpl ListTemplate) Render(w http.ResponseWriter) error {
+
+	// t2 := listTemplate2{ListTemplate: tmpl}
+	//
+	// t2.ActionInputs = make([]map[string]string, len(t2.AdditionalDataInputValues))
+	// for i, v := range t2.AdditionalDataInputValues {
+	// 	t2.ActionInputs[i] = map[string]string{
+	// 		"Key":   t2.AdditionalDataInputName,
+	// 		"Value": v,
+	// 	}
+	// }
+
+	// logg.Debug(tmpl.Rows)
+	// d := ListTemplate{HXGet: "/boxes"}
+	return templates.SafeRender(w, "list", tmpl)
+}
