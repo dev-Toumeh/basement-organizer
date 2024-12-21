53cedd8f0d2724fa515b34245457b05cd7d35ba5
Alex Korn <den.korn.alex@gmail.com>
2024-12-21 12:42:54 +0100

    Implement area list in backend #66

--- internal/areas/area.go
+++ internal/areas/area.go
@@ -2,7 +2,20 @@ package areas
 
 import (
 	"basement/main/internal/common"
+
+	"github.com/gofrs/uuid/v5"
 )
+
+type AreaDatabase interface {
+	CreateArea(newArea Area) (uuid.UUID, error)
+	UpdateArea(box Area) error
+	DeleteArea(boxId uuid.UUID) error
+	AreaById(id uuid.UUID) (Area, error)
+	AreaIDs() ([]uuid.UUID, error)
+	AreaListRows(query string, limit int, page int) ([]common.ListRow, error)
+	AreaListRowByID(id uuid.UUID) (common.ListRow, error)
+	AreaListCounter(searchString string) (count int, err error)
+}
 
 type Area struct {
 	common.BasicInfo
