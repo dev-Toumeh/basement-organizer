b7275cb28881f5c12f4cfb54a79cd48ff9805904
Alex Korn <den.korn.alex@gmail.com>
2025-02-08 09:51:37 +0100

    Separate single/multiple area handlers

--- internal/areas/area-handler.go
+++ internal/areas/area-handler.go
@@ -169,34 +169,3 @@ func createAreaWithID(w http.ResponseWriter, r *http.Request, db AreaDatabase, i
 	server.RedirectWithSuccessNotification(w, "/areas", "Created new area: "+area.Label)
 }
 
-func AreasHandler(db AreaDatabase) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			listPage(db).ServeHTTP(w, r)
-			break
-
-		case http.MethodPost:
-			server.WriteNotImplementedWarning("todo", w, r)
-			break
-
-		case http.MethodDelete:
-			deleteAreas(w, r, db)
-			return
-
-		case http.MethodPut:
-			server.WriteNotImplementedWarning("todo", w, r)
-			break
-
-		default:
-			// Other methods are not allowed.
-			w.Header().Add("Allow", http.MethodGet)
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			server.WriteFprint(w, "Method:'"+r.Method+"' not allowed")
-		}
-	}
-}
-
-func deleteAreas(w http.ResponseWriter, r *http.Request, db AreaDatabase) {
-	server.DeleteThingsFromList(w, r, db.DeleteArea, listPage(db))
-}
