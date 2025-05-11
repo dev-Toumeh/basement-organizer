ff293601fbbd6e21d20b790fc65ab260d8fc1ddc
Naseem Toumeh <dev.toumeh@gmail.com>
2025-05-11 02:14:03 +0200

    refactor: apply new validation package to Area handling logic

--- internal/areas/area.go
+++ internal/areas/area.go
@@ -3,6 +3,7 @@ package areas
 import (
 	"basement/main/internal/common"
 	"basement/main/internal/server"
+	"basement/main/internal/validate"
 	"net/http"
 
 	"github.com/gofrs/uuid/v5"
@@ -24,6 +25,15 @@ type AreaDatabase interface {
 	InnerListRowsFrom2(belongsToTable string, belongsToTableID uuid.UUID, listRowsTable string) ([]common.ListRow, error)
 }
 
+const (
+	ID             string = "id"
+	LABEL          string = "label"
+	DESCRIPTION    string = "description"
+	PICTURE        string = "picture"
+	PREVIEWPICTURE string = "previewpicture"
+	QRCODE         string = "qrcode"
+)
+
 type Area struct {
 	common.BasicInfo
 }
@@ -42,4 +52,43 @@ func areaFromPostFormValue(id uuid.UUID, r *http.Request) (area Area, ignorePict
 	ignorePicture = server.ParseIgnorePicture(r)
 	area.BasicInfo = common.BasicInfoFromPostFormValue(id, r, false)
 	return area, ignorePicture
+}
+
+// ValidateShelf parses form input from an HTTP request,
+// performs inline field-level validation directly within the handler,
+// and returns both the validation result and a Shelf struct if valid.
+// If any validation fails, it returns the validator with error details.
+func ValidateArea(w http.ResponseWriter, r *http.Request) (area Area, validator validate.Validate, err error) {
+	varea := validate.AreaValidate{
+		BasicInfoValidate: validate.BasicInfoValidate{
+			ID:             validate.NewUUIDField(r.PostFormValue(ID)),
+			Label:          validate.NewStringField(r.PostFormValue(LABEL)),
+			Description:    validate.NewStringField(r.PostFormValue(DESCRIPTION)),
+			Picture:        validate.NewStringField(common.ParsePicture(r)),
+			PreviewPicture: validate.NewStringField(common.ParsePicture(r)),
+			QRCode:         validate.NewStringField(r.PostFormValue(QRCODE)),
+		},
+	}
+
+	validator = validate.Validate{Area: varea}
+	if err = validator.ValidateArea(w, varea); err != nil {
+		return area, validator, err
+	}
+
+	if validator.HasValidateErrors() {
+		return area, validator, validator.Err()
+	}
+
+	area = Area{
+		BasicInfo: common.BasicInfo{
+			ID:             varea.ID.UUID(),
+			Label:          varea.Label.String(),
+			Description:    varea.Description.String(),
+			Picture:        varea.Picture.String(),
+			PreviewPicture: varea.PreviewPicture.String(),
+			QRCode:         varea.QRCode.String(),
+		},
+	}
+
+	return area, validator, nil
 }
